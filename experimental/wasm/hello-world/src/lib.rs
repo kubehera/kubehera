65863aa092aacc63bd83598f4eedeca36c5adafb
lic17 <1076366950@qq.com>
2023-04-12 11:46:47 +0800

    fix err of wasm multi value reverse

--- experimental/wasm/hello-world/src/lib.rs
+++ experimental/wasm/hello-world/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(wasm_target_feature)]
+
 use lazy_static::lazy_static;
 use std::sync::Mutex;
 
@@ -6,29 +8,33 @@ lazy_static! {
     static ref WRITE_BUF: Mutex<Vec<u8>> = Mutex::new(vec![0u8;0]);
 }
 
-#[no_mangle]
+/*#[no_mangle]
 pub extern fn get_write_addr() -> *const u8 {
     WRITE_BUF.lock().unwrap().as_ptr()
 }
+*/
 
 #[no_mangle]
-pub unsafe extern fn init_write(len: usize){
+pub unsafe extern fn init_write(len: usize) -> *const u8 {
     let mut write = WRITE_BUF.lock().unwrap();
     *write=Vec::with_capacity(len);
     write.set_len(len);
+    write.as_ptr()
 }
 
 #[no_mangle]
-pub extern fn get_string_len() -> usize{
-    let binding = READ_BUF.lock().unwrap().to_owned();
+pub extern fn get_string() -> (usize, *const u8){
+    let read = READ_BUF.lock().unwrap();
+    let binding = read.to_owned();
     let bytes = binding.as_bytes();
-    bytes.len()
+    return (bytes.len(), read.as_ptr())
 }
 
+/*
 #[no_mangle]
 pub extern fn get_string_addr() ->  *mut u8{
     READ_BUF.lock().unwrap().as_mut_ptr()
-}
+} */
 
 #[no_mangle]
 pub unsafe extern "C" fn greet(){
--- experimental/wasm/wasmtime_hello/src/main.rs
+++ experimental/wasm/wasmtime_hello/src/main.rs
@@ -13,11 +13,9 @@ fn main() -> Result<(),Box<dyn Error>> {
 
 
 //Write Vec[u8] to wasm
-    let write_buffer = "Licheng".as_bytes();
-    let init_write = instance.get_typed_func::<i32, ()>(&mut store, "init_write").unwrap();
-    init_write.call(&mut store,write_buffer.len().try_into().unwrap())?;
-    let get_write_addr = instance.get_typed_func::<(), i32>(&mut store, "get_write_addr").unwrap();
-    let write_addr = get_write_addr.call(&mut store,()).unwrap();
+    let write_buffer = "Licheng123xxx".as_bytes();
+    let init_write = instance.get_typed_func::<i32, i32>(&mut store, "init_write").unwrap();
+    let write_addr = init_write.call(&mut store,write_buffer.len().try_into().unwrap())?;
     memory.write(&mut store,write_addr.try_into().unwrap(),write_buffer)?;
 
 //greet handler
@@ -25,13 +23,9 @@ fn main() -> Result<(),Box<dyn Error>> {
     let _ = greet.call(&mut store,()).unwrap();
 
 //Read String from wasm
-    let get_string_len = instance.get_typed_func::<(), i32>(&mut store, "get_string_len").unwrap();
-    let len = get_string_len.call(&mut store,()).unwrap();
-
+    let get_string = instance.get_typed_func::<(), (i32,i32)>(&mut store, "get_string").unwrap();
+    let (len ,read_addr)= get_string.call(&mut store,()).unwrap();
     let mut read_buffer = vec![0u8; len.try_into().unwrap()];
-
-    let get_string_addr = instance.get_typed_func::<(), i32>(&mut store, "get_string_addr").unwrap();
-    let read_addr = get_string_addr.call(&mut store,()).unwrap();
     memory.read(&mut store, read_addr.try_into().unwrap(), &mut read_buffer).unwrap();
     println!("{}",std::str::from_utf8(&read_buffer[..]).unwrap());
 
